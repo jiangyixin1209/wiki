6075fe99b50aeafb45ec1f6e8e6b16a06b667f45
jiangyx3915 <jyx15221613915@gmail.com>
2021-04-17 15:21:26 +0800

    修改启动图案

--- src/main/java/top/jiangyixihn/wiki/WikiApplication.java
+++ src/main/java/top/jiangyixihn/wiki/WikiApplication.java
@@ -1,13 +1,19 @@
 package top.jiangyixihn.wiki;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.Environment;
 
+@Slf4j
 @SpringBootApplication
 public class WikiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WikiApplication.class, args);
+        SpringApplication application = new SpringApplication(WikiApplication.class);
+        Environment environment = application.run(args).getEnvironment();
+        log.info("启动成功！地址：\thttp://127.0.0.0.1:{}", environment.getProperty("server.port"));
     }
 
 }
